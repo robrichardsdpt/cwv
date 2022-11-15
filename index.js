143fd03ef739f05f13807b5dfdea52b2206d2542
Rob <robrichardsdpt@gmail.com>
2022-11-14 20:08:19 -0500

    added color carousel

--- index.js
+++ index.js
@@ -35,3 +35,18 @@ for (let i = 0; i < coll.length; i++) {
         'url("./bondcliffcopy.jpeg")';
   });
 }
+
+const slidesContainer = document.getElementById("slides-container");
+const slide = document.querySelector(".slide");
+const prevButton = document.getElementById("slide-arrow-prev");
+const nextButton = document.getElementById("slide-arrow-next");
+
+nextButton.addEventListener("click", () => {
+  const slideWidth = slide.clientWidth;
+  slidesContainer.scrollLeft += slideWidth;
+});
+
+prevButton.addEventListener("click", () => {
+  const slideWidth = slide.clientWidth;
+  slidesContainer.scrollLeft -= slideWidth;
+});
